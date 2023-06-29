9f1019699d4bcc4238c04d96085331996e5b59b0
manas <manasksingh@outlook.com>
2023-06-29 19:27:04 +0530

    Added SideBar,Worked on Feed, and added exports to index.js

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import {Box,Stack, Typography} from '@mui/material';
+import { SideBar } from './SideBar';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx = {{ flexDirection: {sx: "column", md: "row"}}}>
+    
+
+      <Box sx= {{height: {sx: 'auto', md: '92vh'}, BorderRight: '1px solid #3s3d3d', px: {sx:0,md:2}}}>
+
+      
+
+        <Typography className= "copyright" variant ="body2" sx = {{mt:1.5, color: '#fff'}}>
+
+          Copyright YT-Clone MS 2023
+
+        </Typography>
+        </Box >
+      </Stack>
   )
 }
+
 
 export default Feed
