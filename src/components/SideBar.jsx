d95de507a41f4795dbf9f9e15f4bd90e69e8ba71
Manasvinitroshan <manasksingh@outlook.com>
2023-07-01 03:07:52 +0530

    push

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Stack} from "@mui/material";
+import { categories } from "../utils/constants";
+//const selectedCategory = 'New';
+
+export const SideBar = () => (
+
+  
+  
+    <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+        {categories.map((category) => (
+
+            <button
+            
+            className="category-btn"
+              >
+                <span>{category.icon}</span>
+                <span>{category.name}</span>
+                
+            </button>
+        ))}
+
+    </Stack>
+  
+    
+    );
+
+   export default SideBar
+
+
